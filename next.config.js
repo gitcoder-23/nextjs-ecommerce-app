0c2a02d2e07a0ddee4841651e45dbe350ba9f572
prabhat-cn <prabhat@capitalnumbers.com>
2022-01-06 16:50:03 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    BASE_URL: 'http://localhost:3000',
+    MONGODB_URL: `mongodb+srv://nextecomm-21:nextecomm-21@cluster0.rplce.mongodb.net/nextjs_ecommerce_app?retryWrites=true&w=majority`,
+    ACCESS_TOKEN_SECRET: 'YOUR_ACCESS_TOKEN_SECRET',
+    REFRESH_TOKEN_SECRET: 'YOUR_REFRESH_TOKEN_SECRET',
+    PAYPAL_CLIENT_ID: 'YOUR_PAYPAL_CLIENT_ID',
+    CLOUD_UPDATE_PRESET: 'YOUR_CLOUD_UPDATE_PRESET',
+    CLOUD_NAME: 'YOUR_CLOUD_NAME',
+    CLOUD_API: 'YOUR_CLOUD_API',
+  },
+};
