08e6333987b33a4834e2f11420162845695f4863
prabhat-cn <prabhat@capitalnumbers.com>
2022-01-18 19:10:58 +0530

    ⚡️

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@ module.exports = {
   env: {
     BASE_URL: 'http://localhost:3000',
     MONGODB_URL: `mongodb+srv://nextecomm-21:nextecomm-21@cluster0.rplce.mongodb.net/nextjs_ecommerce_app?retryWrites=true&w=majority`,
-    ACCESS_TOKEN_SECRET: 'YOUR_ACCESS_TOKEN_SECRET',
-    REFRESH_TOKEN_SECRET: 'YOUR_REFRESH_TOKEN_SECRET',
+    ACCESS_TOKEN_SECRET: `n#RMW2MLE)>h;b@,ch)'wMnD/FXZ<@-g@@kpH8{:=ZPyd8Eb]f`,
+    REFRESH_TOKEN_SECRET:
+      'K!z>`P4EF:k+-^8]ge9:B3wwS85(=8zs7%j3ux{U*y})Y%4/Q"Le_*L/~arQ?w5(v$YY7#X*X}=QT,{WJk2p}t+""5N{Q!/!/sQT',
     PAYPAL_CLIENT_ID: 'YOUR_PAYPAL_CLIENT_ID',
-    CLOUD_UPDATE_PRESET: 'YOUR_CLOUD_UPDATE_PRESET',
-    CLOUD_NAME: 'YOUR_CLOUD_NAME',
-    CLOUD_API: 'YOUR_CLOUD_API',
+    CLOUD_UPDATE_PRESET: 'imPIOdLNH8n-QYU-_u16qUIVWQg',
+    CLOUD_NAME: 'drcloud21',
+    CLOUD_API: '553796143519187',
   },
 };
